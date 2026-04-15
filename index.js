1d3d2be80c22ceb50894f2ed50662d7b0aa8683b
Priyanshi-max <priyanshijhanwar05@gmail.com>
2026-04-15 13:34:10 +0530

    Cors issue

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(helmet());
 //   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // }));
-const cors = require("cors");
+
 
 app.use(cors({
   origin: [
